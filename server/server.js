0fc61ae15d00bbf94928f8983982d01a2adc7a8c
Natalia Teaca <lilikoy@sbcglobal.net>
2020-10-08 14:16:15 -0700

    apolloprovider add

---
+++ server/server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+const db = require('./config/connection');
+const routes = require('./routes');
+const { typeDefs, resolvers } = require('./schemas');
+// Apollo & Middleware
+const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+// ApolloServer for schema data
+ const server = new ApolloServer({ typeDefs, resolvers, contect: authMiddleware});
+
+ server.applyMiddleware({ app });
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+});
