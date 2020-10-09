91fae9b7dd18d8c6d8dda36ba0fe8a3d74a91057
Natalia Teaca <lilikoy@sbcglobal.net>
2020-10-09 12:07:58 -0700

    mongo issue

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose');
+
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/book-club',
   {
@@ -7,3 +9,5 @@ mongoose.connect(
     useFindAndModify: false
   }
 );
+
+module.exports = mongoose.connection;
